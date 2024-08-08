f2f0220615bfd6646f750dbd419e31e3d5f4800d
Arthur Santana <arthurgn.santana@gmail.com>
2024-08-08 10:27:27 -0300

    fix: Refactor ConnectionContext to use generic DbContextOptions

--- Training.Api/Infrastructure/ConnectionContext.cs
+++ Training.Api/Infrastructure/ConnectionContext.cs
@@ -4,12 +4,7 @@ using Training.Api.Entities;
 
 namespace Training.Api.Infrastructure;
 
-public class ConnectionContext(DbContextOptions options) : DbContext(options)
+public class ConnectionContext(DbContextOptions<ConnectionContext> options) : DbContext(options)
 {
     public DbSet<Employee> Employee { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        optionsBuilder.UseNpgsql("Server=localhost;Database=Training;User Id=postgres;Password=password;");
-    }
 }
